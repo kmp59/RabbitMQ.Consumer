4847766f815d3e308d69c408c4158cecac97d119
kmp59 <kmp59@njit.edu>
2021-09-02 01:20:41 -0400

    Initial Code Commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# RabbitMQ.Consumer
-RabbitMQ Consumer app that creates the Quorum Queue and attaches to it, in order to listen to the incoming messages.
+<div align="center">
+
+  # RabbitMQ.Consumer
+
+</div>
+
+<h4>RabbitMQ Consumer app that creates the Quorum Queue and attaches to it, in order to listen to the incoming messages.</h4>
+
+1. Creates connection with local instance of RabbitMQ
+2. Creates the Queue if already doesn't exists
+3. Receives messages from the Queue and parse it back to string
---
+++ RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMQ.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri("amqp://guest:guest@localhost:5672")
+            };
+            using var conn = factory.CreateConnection();
+            using(var channel = conn.CreateModel())
+            {
+                var arguments = new Dictionary<string, object> {};
+                arguments.Add("x-queue-type", "quorum");
+                channel.QueueDeclare(queue: "demo-queue", durable: true, exclusive: false, autoDelete: false, arguments: arguments);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (sender, e) =>
+                {
+                    var body = e.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(message);
+                };
+                channel.BasicConsume("demo-queue", true, consumer);
+                Console.ReadLine();
+            }
+        }
+    }
+}
